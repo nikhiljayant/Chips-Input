ac70a9498110ddbf08bf4d2f6a17a9eda8edd0fb
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-05-16 23:38:33 +0530

    feat: enhance App component layout and add custom font styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 function App() {
-  return <></>;
+  return (
+    <div className="flex flex-col gap-[20px] items-center justify-center h-[100vh] w-full font-bricolage">
+      <h1 className="text-3xl font-medium">Chips Input</h1>
+
+      <input placeholder="Enter Chips and Press Enter" className="border rounded-md py-[5px] px-[10px] w-[300px] placeholder:text-center" />
+    </div>
+  );
 }
 
 export default App;
