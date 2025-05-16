9437e9c386b8657c344f0169d8ca230a35c6639f
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-05-16 23:55:22 +0530

    feat: implement chips input functionality with state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 function App() {
+  const [input, setInput] = useState("");
+  const [chips, setChips] = useState([]);
   return (
     <div className="flex flex-col gap-[20px] items-center justify-center h-[100vh] w-full font-bricolage">
       <h1 className="text-3xl font-medium">Chips Input</h1>
 
-      <input placeholder="Enter Chips and Press Enter" className="border rounded-md py-[5px] px-[10px] w-[300px] placeholder:text-center" />
+      <input
+        placeholder="Enter Chips and Press Enter"
+        className="border rounded-md py-[5px] px-[10px] w-[300px] placeholder:text-center"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && input !== "") {
+            setChips([...chips, { id: "", name: input }]);
+            setInput("");
+          }
+        }}
+      />
+
+      {chips?.length > 0 &&
+        chips?.map((chip) => <div key={chip?.id}>{chip?.name}</div>)}
     </div>
   );
 }
